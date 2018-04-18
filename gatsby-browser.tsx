2c603ccf641a7df7d9dd6c7f4109516f7fbce569
Alvis HT Tang <alvis@hilbert.space>
2018-04-18 10:06:32 +0100

    template(browser): provide a gatsby-browser template

---
+++ gatsby-browser.tsx
@@ -0,0 +1,44 @@
+/**
+ *                            *** MIT LICENSE ***
+ * -------------------------------------------------------------------------
+ * This code may be modified and distributed under the MIT license.
+ * See the LICENSE file for details.
+ * -------------------------------------------------------------------------
+ *
+ * @summary   The following script defines actions to
+ *            be loaded in the clients' browser.
+ *
+ *            See https://www.gatsbyjs.org/docs/browser-apis
+ *            for detailed usage.
+ *
+ * @author    Alvis HT Tang <alvis@hilbert.space>
+ * @license   MIT
+ * @copyright Copyright (c) 2018 - All Rights Reserved.
+ * -------------------------------------------------------------------------
+ */
+
+// import Browser from '#definitions/gatsby/browser';
+
+// export const onClientEntry: Browser.onClientEntry = (): void => {};
+
+// export const onInitialClientRender: Browser.onInitialClientRender = (): void => {};
+
+// export const onRouteUpdate: Browser.onRouteUpdate = (parameters): void => {};
+
+// export const registerServiceWorker: Browser.registerServiceWorker = (): boolean => {};
+
+// export const replaceComponentRenderer: Browser.replaceComponentRenderer = (parameters): void => {};
+
+// export const replaceHydrateFunctionL: Browser.replaceHydrateFunction = (): ((
+//   element: JSX.Element,
+//   container: Element,
+//   callback: () => void
+// ) => void) => {};
+
+// export const replaceHistory: Browser.replaceHistory = (): History => {};
+
+// export const replaceRouterComponent: Browser.replaceRouterComponent = (parameters): React.ReactNode => {};
+
+// export const shouldUpdateScroll: Browser.shouldUpdateScroll = (parameters): void => {};
+
+// export const wrapRootComponent: Browser.wrapRootComponent = (parameters): void => {};
