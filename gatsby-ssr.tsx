db55c17389f164e9fa7023df5a24be6e1b7d9b2d
Alvis HT Tang <alvis@hilbert.space>
2018-04-25 17:34:04 +0100

    template(ssr): provide a gatsby-ssr template

---
+++ gatsby-ssr.tsx
@@ -0,0 +1,24 @@
+/**
+ *                            *** MIT LICENSE ***
+ * -------------------------------------------------------------------------
+ * This code may be modified and distributed under the MIT license.
+ * See the LICENSE file for details.
+ * -------------------------------------------------------------------------
+ *
+ * @summary   The code below specifies the configuration
+ *            related to the server-side rendering.
+ *
+ *            See https://www.gatsbyjs.org/docs/ssr-apis
+ *            for defailed usage
+ *
+ * @author    Alvis HT Tang <alvis@hilbert.space>
+ * @license   MIT
+ * @copyright Copyright (c) 2018 - All Rights Reserved.
+ * -------------------------------------------------------------------------
+ */
+
+// import SSR from '#definitions/gatsby/ssr';
+
+// export const onRenderBody: SSR.onRenderBody = (parameters): void => {};
+
+// export const replaceRenderer: SSR.replaceRenderer = (parameters): void => {};
