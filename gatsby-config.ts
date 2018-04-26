913f025c0e34e2e049b1eab669e24a52684d201b
Alvis HT Tang <alvis@hilbert.space>
2018-04-26 19:33:00 +0100

    feat(config): provide a basic configuration for a useful webapp

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -16,6 +16,25 @@
  * -------------------------------------------------------------------------
  */
 
-// import * as Config from '#definitions/gatsby/config';
+import Config from '#definitions/gatsby/config';
 
-// export const plugins: Config.Plugins = [];
+export const plugins: Config.Plugins = [
+  `gatsby-plugin-catch-links`, // catch links in markdown files
+  {
+    resolve: `gatsby-plugin-nprogress`, // show the nprogress indicator when a page is delayed in loading
+    options: {
+      color: `tomato`
+    }
+  },
+  `gatsby-plugin-offline`, // enable offline browsing
+  {
+    resolve: `gatsby-plugin-sass`, // enable postcss + sass
+    options: {
+      postCssPlugins: []
+    }
+  },
+  `gatsby-plugin-purify-css`, // remove unused CSS in production
+  `gatsby-plugin-react-helmet`, // ensure the custom style sheet is inserted properly
+  `gatsby-plugin-sitemap`, // generate a sitemap automatically
+  `gatsby-plugin-typescript` // enable typescript
+];
