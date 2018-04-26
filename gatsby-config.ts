d6ec13d4cc696cef63a736d304dfa4ff0ca2c75e
Alvis HT Tang <alvis@hilbert.space>
2018-04-26 19:39:47 +0100

    template(config): provide a gatsby-config template

---
+++ gatsby-config.ts
@@ -0,0 +1,21 @@
+/**
+ *                            *** MIT LICENSE ***
+ * -------------------------------------------------------------------------
+ * This code may be modified and distributed under the MIT license.
+ * See the LICENSE file for details.
+ * -------------------------------------------------------------------------
+ *
+ * @summary   The code below specifies the configuration for Gatsby.
+ *
+ *            See https://www.gatsbyjs.org/docs/gatsby-config
+ *            for defailed usage
+ *
+ * @author    Alvis HT Tang <alvis@hilbert.space>
+ * @license   MIT
+ * @copyright Copyright (c) 2018 - All Rights Reserved.
+ * -------------------------------------------------------------------------
+ */
+
+// import * as Config from '#definitions/gatsby/config';
+
+// export const plugins: Config.Plugins = [];
