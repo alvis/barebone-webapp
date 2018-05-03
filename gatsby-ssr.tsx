68809122504cbac98d92ad6b23f85242746d1b50
Alvis HT Tang <alvis@hilbert.space>
2018-05-03 11:03:03 +0100

    fix(redux): provide a workaround for redux not working with glamor
    fix a problem of `WebpackError: Could not find "store"`
    see https://github.com/gatsbyjs/gatsby/issues/3231

--- gatsby-ssr.tsx
+++ gatsby-ssr.tsx
@@ -17,8 +17,47 @@
  * -------------------------------------------------------------------------
  */
 
-// import SSR from '#definitions/gatsby/ssr';
+import { renderStaticOptimized } from 'glamor/server';
+import React from 'react';
+import { renderToString } from 'react-dom/server';
+import { Provider } from 'react-redux';
+
+import createStore from '#redux';
+
+import SSR from '#definitions/gatsby/ssr';
 
 // export const onRenderBody: SSR.onRenderBody = (parameters): void => {};
 
-// export const replaceRenderer: SSR.replaceRenderer = (parameters): void => {};
+// fix https://github.com/gatsbyjs/gatsby/issues/3231
+export const replaceRenderer: SSR.replaceRenderer = ({
+  bodyComponent,
+  replaceBodyHTMLString,
+  setHeadComponents
+}): void => {
+  const { store } = createStore();
+
+  const { html, css, ids } = renderStaticOptimized(() =>
+    renderToString(<Provider store={store}>{bodyComponent}</Provider>)
+  );
+
+  replaceBodyHTMLString(html);
+
+  setHeadComponents([
+    <style
+      id="glamor-styles"
+      key="glamor-styles"
+      dangerouslySetInnerHTML={{ __html: css }}
+    />,
+    <script
+      id="glamor-ids"
+      key="glamor-ids"
+      dangerouslySetInnerHTML={{
+        __html: `
+        // <![CDATA[
+        window._glamor = ${JSON.stringify(ids)}
+        // ]]>
+        `
+      }}
+    />
+  ]);
+};
